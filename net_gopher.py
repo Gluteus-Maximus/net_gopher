0ec804d8512d3f65c89631bf0bc3b74f2bd79024
Shea, Derek <derek.t.shea@gmail.com>
2019-12-29 08:42:51 +0000

    Add parser and several custom actions.

--- net_gopher.py
+++ net_gopher.py
@@ -2,12 +2,15 @@
 '''
 For Nix Systems Only.
 '''
+import sys
 import os
-import pathlib
+import argparse as ap
 import re
 import time
+import pathlib
 import csv
 import subprocess as sp
+import copy as _copy
 
 #TODO: capture output from scripts
 
@@ -26,13 +29,74 @@ class Credentials():
     self.password = password
 
 
+def main():
+  args = getargs()
+  print(args)
+
+
 def getargs():
   #TODO: how to allow multiple of a flag?
   #d = defaults.copy()
   #d.update(os.environ)
   #d.update(command_line_args)
   #ChainMap
-  pass
+  parser = ap.ArgumentParser()  #TODO: add desc
+  parser.add_argument('-r', '--remoteCreds', action=readable_file, required=True)
+  parser.add_argument('-b', '--bashScript', dest='bashScripts',
+      action=readable_file_append, required=True)
+  parser.add_argument('--test', type=int, required=True)
+  #parser.add_argument('', required=True)
+  #parser.add_argument('', required=True)
+  #TODO: get gate password securely? file with permission restrictions?
+  #TODO: support non-standard key formatters (-f nargs=2 key,value - append to formatters)
+  #TODO: default values for standard key formatters
+  try:
+    return parser.parse_args()
+  except ap.ArgumentTypeError as e:
+    #TODO: print usage
+    print(e, file=sys.stderr)
+    exit(1)
+
+
+class readable_dir(ap.Action):
+  def __call__(self, parser, namespace, values, option_string=None):
+    prospective_dir=values
+    if not os.path.isdir(prospective_dir):
+      raise ap.ArgumentTypeError("error: {0} is not a valid path".format(prospective_dir))
+    if os.access(prospective_dir, os.R_OK):
+      setattr(namespace,self.dest,prospective_dir)
+    else:
+      raise ap.ArgumentTypeError("error: {0} is not a readable dir".format(prospective_dir))
+
+
+class readable_file(ap.Action):
+  def __call__(self, parser, namespace, values, option_string=None):
+    prospective_file=values
+    if not os.path.isfile(prospective_file):
+      raise ap.ArgumentTypeError("error: {0} is not a valid file".format(prospective_file))
+    if os.access(prospective_file, os.R_OK):
+      setattr(namespace,self.dest,prospective_file)
+    else:
+      raise ap.ArgumentTypeError("error: {0} is not a readable file".format(prospective_file))
+
+
+class readable_file_append(ap.Action):
+  def __call__(self, parser, namespace, values, option_string=None):
+    prospective_file=values
+    if not os.path.isfile(prospective_file):
+      raise ap.ArgumentTypeError("error: {0} is not a valid file".format(prospective_file))
+    if os.access(prospective_file, os.R_OK):
+      items = _copy.copy(_ensure_value(namespace, self.dest, []))
+      items.append(prospective_file)
+      setattr(namespace, self.dest, items)
+    else:
+      raise ap.ArgumentTypeError("error: {0} is not a readable file".format(prospective_file))
+
+
+def _ensure_value(namespace, name, value):
+    if getattr(namespace, name, None) is None:
+        setattr(namespace, name, value)
+    return getattr(namespace, name)
 
 
 def parse_csv(csvFile):
@@ -167,3 +231,7 @@ def log_session_data_raw(ip, data, logfile):
 
 def log_session_data_json():
   pass
+
+
+if __name__ == "__main__":
+  main()
